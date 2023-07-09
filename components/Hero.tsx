5f43a0c3312aef15b35c6bdcd4d884419c2e18a2
Zefanja Timorason <z.timorason@gmail.com>
2023-07-09 23:10:45 +0200

    started on the hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+
+ const Hero = () => {
+  return (
+      <div className="hero">
+          <div className="flex-1 pt-36 padding-x">
+              <h1 className="hero__title">
+                  Find, rent or book a car!
+              </h1>
+              <p className="hero__subtitle"> Streamline your car rental experience with our effortless booking process.</p>
+              <CustomButton />
+          </div>
+    </div>
+  )
+}
+
+export default Hero
