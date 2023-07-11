87704278ee8658b7e09b277985ac4328cd12d4b3
Zefanja Timorason <z.timorason@gmail.com>
2023-07-11 14:48:14 +0200

    added the second api

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import CustomButton from "./CustomButton";
 import CarDetails from "./CarDetails";
 
@@ -30,7 +30,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className="relative w-full object-contain h-40 my-3">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car-model"
           fill
           priority
